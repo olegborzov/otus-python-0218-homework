b1a777bbbea8e96d5e9d0a0e38450189b481e354
olegborzov <mail@olegborzov.ru>
2018-03-05 19:34:11 +0300

    upload hw1

---
+++ hw1/log_parser.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import gzip
+from datetime import datetime
+
+
+def parse_log_file(log_path):
+    """
+    Parse log file and prepare dict for analyze
+    """
+    is_gzipped = log_path.endswith(".gz")
+    opener = gzip.open(log_path, "r") if is_gzipped else open(log_path, "r", encoding="UTF-8")
+
+    log_urls = {"errors": 0, "urls_times": {}}
+    with opener:
+        for line in opener:
+            if is_gzipped:
+                line = line.decode("UTF-8")
+            line_parsed = parse_log_line(line)
+            if line_parsed["is_error"]:
+                log_urls["errors"] += 1
+            else:
+                if line_parsed["url"] not in log_urls["urls_times"]:
+                    log_urls["urls_times"][line_parsed["url"]] = []
+                log_urls["urls_times"][line_parsed["url"]].append(line_parsed["request_time"])
+
+    return log_urls
+
+
+def parse_log_line(line):
+    """
+    Parse url and request time from log line
+    """
+    log_info = {"url": "", "is_error": True, "request_time": 0}
+
+    parsed_line = re.findall(r"\"[A-Z]+ ([^\s]+) .* (\d+\.\d+)\n", line)
+
+    if not parsed_line:
+        return log_info
+
+    log_info["url"] = parsed_line[0][0]
+    log_info["request_time"] = float(parsed_line[0][1])
+
+    if log_info["url"] and log_info["request_time"]:
+        log_info["is_error"] = False
+
+    return log_info
+
+
+def get_newest_log_file(log_dir):
+    """
+    Get newest ui_log file in log_dir
+    """
+    fileslist = os.listdir(log_dir)
+    last_log = get_newest_file_from_list(fileslist, log_dir)
+
+    return last_log
+
+
+def get_newest_file_from_list(fileslist, log_dir):
+    last_log = {"date": None, "filepath": None}
+    for filename in fileslist:
+        log_date = get_log_file_date(filename)
+        if log_date:
+            if not last_log["date"] or log_date > last_log["date"]:
+                last_log["date"] = log_date
+                last_log["filepath"] = os.path.join(log_dir, filename)
+
+    return last_log
+
+
+def get_log_file_date(filename):
+    """
+    Get date from log filename
+    """
+    log_date = None
+    try:
+        log_date_str = re.findall(r"nginx-access-ui\.log-(\d{8})(\.gz)?", filename)
+        if log_date_str:
+            log_date = datetime.strptime(log_date_str[0][0], "%Y%m%d").date()
+
+        return log_date
+    except ValueError:
+        return log_date
---
+++ hw1/main.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+import json
+import logging
+from datetime import datetime
+
+import log_parser
+import log_analyzer
+import report_generator
+import project_exceptions
+
+# log_format ui_short '$remote_addr $remote_user $http_x_real_ip [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+#                     '$request_time';
+
+DEFAULT_CONFIG = {
+    "REPORT_SIZE": 1000,
+    "REPORT_DIR": "./reports",
+    "LOG_DIR": "./log",
+    "LOG_RESULT_DIR": "./log_result",
+    "TS_DIR": "./",
+    "MAX_LOG_ERRORS_PERCENT": 25
+}
+DEFAULT_CONFIG_PATH = "./config.json"
+
+
+def get_config_path_from_args(default_config_path):
+    """
+    Setup args for command line, read args
+    :param default_config_path: path to default config file
+    :return: path to config file
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, help="path to config file, example: /home/me/config.json")
+    args = parser.parse_args()
+
+    config_path = args.config if args.config else default_config_path
+    return config_path
+
+
+def parse_config(path, default_config):
+    """
+    Parse config from json file by path and concatenate with default config
+    :return: result config
+    """
+    try:
+        with open(path) as f:
+            config = json.load(f)
+
+        assert type(config) is dict
+
+        if not config:
+            return default_config
+
+        for k in default_config.keys():
+            if k in config:
+                default_config[k] = config[k]
+
+        return default_config
+    except (json.JSONDecodeError, FileNotFoundError, AssertionError) as ex:
+        raise project_exceptions.ParseConfigError(ex)
+
+
+def set_logging(log_dir):
+    """
+    Set logging config - to file if is set log_dir, else - to stdout
+    """
+    log_file = None
+
+    if log_dir and os.access(log_dir, os.W_OK):
+        log_file_name = datetime.now().strftime("log_analyzer_%Y%m%d_%H%M%S.log")
+        log_file = os.path.join(log_dir, log_file_name)
+
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+    )
+
+
+def update_ts(ts_dir):
+    """
+    Update timestamp file
+    :param ts_dir: path to dir, where ts file will be stored
+    """
+    ts_path = os.path.join(ts_dir, "log_analyzer.ts")
+
+    now = datetime.now()
+    timestamp = int(now.timestamp())
+    with open(ts_path, "w", encoding="UTF-8") as f:
+        f.write(str(timestamp))
+
+    os.utime(ts_path, times=(timestamp, timestamp))
+
+
+def main():
+    try:
+        # 1. Prepare
+        set_logging(None)
+
+        config_path = get_config_path_from_args(DEFAULT_CONFIG_PATH)
+        config = parse_config(config_path, DEFAULT_CONFIG)
+
+        set_logging(config["LOG_RESULT_DIR"])
+
+        # 2. Parse log
+        last_log_file_info = log_parser.get_newest_log_file(config["LOG_DIR"])
+        if not last_log_file_info["filepath"]:
+            logging.info("No log file found in dir {}".format(config["LOG_DIR"]))
+            return
+
+        if report_generator.report_by_date_exists(last_log_file_info["date"], config["REPORT_DIR"]):
+            logging.info("Report for {} already exists".format(last_log_file_info["date"]))
+            return
+
+        log_urls = log_parser.parse_log_file(last_log_file_info["filepath"])
+        if not log_urls:
+            logging.info("Log file ({}) is empty".format(last_log_file_info["filepath"]))
+            return
+        logging.info("Parsed {lines} lines, {urls} urls from log file".format(
+            lines=log_analyzer.calc_log_rows_count(log_urls),
+            urls=len(log_urls["urls_times"])
+        ))
+
+        # 3. Analyze log
+        report_list = log_analyzer.analyze_log(log_urls, config["MAX_LOG_ERRORS_PERCENT"], config["REPORT_SIZE"])
+        logging.info("Log has been analyzed")
+
+        # 4. Generate report
+        report_path = report_generator.save_report_html(report_list, last_log_file_info["date"], config["REPORT_DIR"])
+
+        update_ts(config["TS_DIR"])
+
+        logging.info("Log file ({log_path}) parsed succesfully. Created report file - {report_path}".format(
+            log_path=last_log_file_info["filepath"],
+            report_path=report_path
+        ))
+        # 4. TODO: Write tests
+    except Exception as ex:
+        msg = "{0}: {1}".format(type(ex).__name__, ex)
+        logging.exception(msg, exc_info=True)
+        raise
+
+
+if __name__ == "__main__":
+    main()
